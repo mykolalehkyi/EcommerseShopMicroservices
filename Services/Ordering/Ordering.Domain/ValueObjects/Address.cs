eed27234fe2e008127ee7e49a5347cf1a63efbcf
Mykola Lehkyi <mykola.lehkyi.dev@gmail.com>
2024-07-03 16:54:21 +0300

    Added value objects domain logic

--- Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
+++ Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -16,5 +16,27 @@ namespace Ordering.Domain.ValueObjects
         public string State { get; private set; } = default!;
         public string ZipCode { get; private set; } = default!;
 
+        protected Address()
+        {
+        }
+
+        private Address(string firstName, string lastName, string? emailAddress, string addressLine, string country, string state, string zipCode)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = emailAddress;
+            AddressLine = addressLine;
+            Country = country;
+            State = state;
+            ZipCode = zipCode;
+        }
+
+        public static Address Of(string firstName, string lastName, string? emailAddress, string addressLine, string country, string state, string zipCode)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(emailAddress);
+            ArgumentException.ThrowIfNullOrWhiteSpace(addressLine);
+
+            return new Address(firstName, lastName, emailAddress, addressLine, country, state, zipCode);
+        }
     }
 }
--- Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
+++ Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Net.Mail;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,5 +17,27 @@ namespace Ordering.Domain.ValueObjects
         public string CVV { get; private set; } = default!;
         public int PaymentMethod { get; private set; } = default!;
 
+        protected Payment()
+        {
+        }
+
+        private Payment(string? cardName, string cardNumber, string expiration, string cVV, int paymentMethod)
+        {
+            CardName = cardName;
+            CardNumber = cardNumber;
+            Expiration = expiration;
+            CVV = cVV;
+            PaymentMethod = paymentMethod;
+        }
+
+        public static Payment Of(string? cardName, string cardNumber, string expiration, string cVV, int paymentMethod)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cVV);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(cVV.Length, 3);
+
+            return new Payment(cardName,cardNumber,expiration,cVV,paymentMethod);
+        }
     }
 }
